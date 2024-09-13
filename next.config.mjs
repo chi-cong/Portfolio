858fc301d710ba24d7e1890c29b4e29ccbfd00bc
Chi Cong <chicong422002@gmail.com>
2024-09-14 01:29:52 +0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/portfolio",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
